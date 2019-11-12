793a32a77070c3a709af4aac7cd54394ff771723
Andrew Nunez <22259@columbushs.com>
2019-11-12 14:45:05 -0500

    almost done

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,14 +1,41 @@
 // Rover Object Goes Here
 // ======================
-
+let rover ={
+direction: 'N'  
+}
 // ======================
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+ if(rover.direction == 'N'){
+  rover.direction = 'W';
+ }
+ else if (rover.direction == 'W'){
+  rover.direction = 'S'
+ }
+ else if (rover.direction == 'S'){
+  rover.direction = 'E'
+ }
+ else if (rover.direction == 'E'){
+  rover.direction = 'N'
+ } 
+ console.log("turnRight was called!");
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+ if(rover.direction == 'N'){
+  rover.direction = 'E';
+ }
+ else if (rover.direction == 'E'){
+  rover.direction = 'S'
+ }
+ else if (rover.direction == 'S'){
+  rover.direction = 'W'
+ }
+ else if (rover.direction == 'W'){
+  rover.direction = 'N'
+ }
+ console.log("turnRight was called!");
 }
+
 
 function moveForward(rover){
   console.log("moveForward was called")
